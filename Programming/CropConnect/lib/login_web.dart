1c6f61d38166b2c5499b3f53538d6b732adce4eb
Alex Stoia <alexstoia.as@gmail.com>
2024-02-19 17:02:46 +0100

    Visuals

--- Programming/CropConnect/lib/login_web.dart
+++ Programming/CropConnect/lib/login_web.dart
@@ -44,180 +44,199 @@ class _LoginPageState extends State<WebLoginPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Stack(
-          children: [
-            // Imagen que ocupa toda la pantalla
-            Positioned.fill(
-              child: Image.asset(
-                "images/campo.jpg",
-                fit: BoxFit.cover,
-              ),
-            ),
-
-            // Formulario de inicio de sesión superpuesto en la parte derecha
-            Positioned(
-              top: 0,
-              right: 0,
-              bottom: 0,
-              child: Padding(
-                padding: const EdgeInsets.only(right: 45, top: 40, bottom: 40),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    gradient: const LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomRight,
-                      stops: [0.12, 1],
-                      colors: [
-                        Colors.white,
-                        Colors.white70,
-                      ],
-                    ),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Stack(
+              children: [
+                // Imagen que ocupa toda la pantalla
+                Positioned.fill(
+                  child: Image.asset(
+                    "images/campo.jpg",
+                    fit: BoxFit.cover,
                   ),
-                  width: MediaQuery.of(context).size.width * 0.25,
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // AppName y logo
-                      const Padding(
-                        padding: EdgeInsets.only(top: 100),
-                        child: Text(
-                          'Welcome to CropConnect!',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontFamily: "SanFrancisco",
-                            color: Colors.black,
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: -3,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-                      Center(
-                        child: SvgPicture.asset(
-                          "./images/logo.svg",
-                          semanticsLabel: "CropConnect Logo",
-                          height: 150,
-                        ),
-                      ),
-                      const SizedBox(height: 30),
+                ),
 
-                      // Email textfield
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: TextField(
-                          controller: _emailController,
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.grey),
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: AppColors.darkGreen),
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            hintText: AutofillHints.email,
-                            hintStyle: const TextStyle(
-                              fontFamily: "SanFranciso",
-                            ),
-                            fillColor: Colors.grey[100],
-                            filled: true,
-                          ),
+                // Formulario de inicio de sesión superpuesto en la parte derecha
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 45, top: 40, bottom: 40),
+                    child: Container(
+                      width: constraints.maxWidth * 0.25,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        gradient: const LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomRight,
+                          stops: [0.12, 1],
+                          colors: [
+                            Colors.white,
+                            Colors.white70,
+                          ],
                         ),
                       ),
-                      const SizedBox(height: 12),
-
-                      // Password textfield
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: TextField(
-                          obscureText: true,
-                          controller: _passwordController,
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(color: Colors.grey),
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: AppColors.darkGreen),
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            hintText: AutofillHints.password,
-                            fillColor: Colors.grey[200],
-                            filled: true,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 12),
-
-                      // SignIn button
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: GestureDetector(
-                          onTap: signIn,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12),
-                              gradient: LinearGradient(
-                                begin: Alignment.topRight,
-                                end: Alignment.bottomLeft,
-                                stops: const [0.4, 0.6],
-                                colors: [
-                                  AppColors.darkGreen2,
-                                  AppColors.darkGreen,
-                                ],
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // AppName y logo
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: constraints.maxHeight * 0.1,
+                              ),
+                              child: Text(
+                                'Welcome to CropConnect!',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontFamily: "SanFrancisco",
+                                  color: Colors.black,
+                                  fontSize: constraints.maxWidth < 600 ? 25 : 50,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: -3,
+                                ),
                               ),
                             ),
-                            child: const Padding(
-                              padding: EdgeInsets.only(top: 20, bottom: 20),
-                              child: Center(
-                                child: Text(
-                                  'Sign In',
-                                  style: TextStyle(
-                                    fontFamily: "SanFrancisco",
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 30,
+                            SizedBox(height: constraints.maxWidth * 0.01),
+                            Center(
+                              child: SvgPicture.asset(
+                                "./images/logo.svg",
+                                semanticsLabel: "CropConnect Logo",
+                                height: constraints.maxWidth < 600 ? 80 : 150,
+                              ),
+                            ),
+                            SizedBox(height: constraints.maxHeight * 0.05),
+
+                            // Email textfield
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: constraints.maxWidth < 600 ? 10 : 20,
+                              ),
+                              child: TextField(
+                                controller: _emailController,
+                                decoration: InputDecoration(
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.grey),
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide:
+                                    BorderSide(color: AppColors.darkGreen),
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  hintText: AutofillHints.email,
+                                  hintStyle: TextStyle(
+                                    fontFamily: "SanFranciso",
+                                  ),
+                                  fillColor: Colors.grey[100],
+                                  filled: true,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 12),
+
+                            // Password textfield
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: constraints.maxWidth < 600 ? 10 : 20,
+                              ),
+                              child: TextField(
+                                obscureText: true,
+                                controller: _passwordController,
+                                decoration: InputDecoration(
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.grey),
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide:
+                                    BorderSide(color: AppColors.darkGreen),
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  hintText: AutofillHints.password,
+                                  fillColor: Colors.grey[200],
+                                  filled: true,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 12),
+
+                            // SignIn button
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: constraints.maxWidth < 600 ? 10 : 20,
+                              ),
+                              child: GestureDetector(
+                                onTap: signIn,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(12),
+                                    gradient: LinearGradient(
+                                      begin: Alignment.topRight,
+                                      end: Alignment.bottomLeft,
+                                      stops: const [0.4, 0.6],
+                                      colors: [
+                                        AppColors.darkGreen2,
+                                        AppColors.darkGreen,
+                                      ],
+                                    ),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                      top: 20,
+                                      bottom: 20,
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'Sign In',
+                                        style: TextStyle(
+                                          fontFamily: "SanFrancisco",
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize:
+                                          constraints.maxWidth < 600 ? 20 : 30,
+                                        ),
+                                      ),
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
+                            SizedBox(height: 20),
+
+                            // Register button
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Not a Member? ",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    // TODO: change to create a redirect to mail
+                                  },
+                                  child: Text(
+                                    "Contact Us",
+                                    style: TextStyle(
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
-                      const SizedBox(height: 20),
-
-                      // Register button
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            "Not a Member? ",
-                            style: TextStyle(
-                              color: Colors.black,
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              // TODO: change to create a redirect to mail
-                            },
-                            child: const Text(
-                              "Contact Us",
-                              style: TextStyle(
-                                color: Colors.blue,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ],
+              ],
+            );
+          },
         ),
       ),
     );
--- Programming/CropConnect/lib/web_home_page.dart
+++ Programming/CropConnect/lib/web_home_page.dart
@@ -55,7 +55,7 @@ class _WebHomePageState extends State<WebHomePage> {
                       gradientStartColor: AppColors.darkGreen2,
                       gradientEndColor: AppColors.darkGreen,
                       iconRoute: "images/what_to_plant.png",
-                      redirectScreen: LoginPage(),
+                      redirectScreen: AppLoginPage(),
                     ),
                     SizedBox(height: separation),
                      ActionsBox(
@@ -64,7 +64,7 @@ class _WebHomePageState extends State<WebHomePage> {
                        gradientStartColor: AppColors.darkGreen2,
                        gradientEndColor: AppColors.darkGreen,
                       iconRoute: "images/800px-Circle_-_black_simple.svg.png",
-                      redirectScreen: LoginPage(),
+                      redirectScreen: AppLoginPage(),
                     ),
                     SizedBox(height: separation),
                      ActionsBox(
@@ -73,7 +73,7 @@ class _WebHomePageState extends State<WebHomePage> {
                        gradientStartColor: AppColors.darkGreen2,
                        gradientEndColor: AppColors.darkGreen,
                       iconRoute: "images/800px-Circle_-_black_simple.svg.png",
-                      redirectScreen: LoginPage(),
+                      redirectScreen: AppLoginPage(),
                     ),
                     SizedBox(height: separation),
                      ActionsBox(
@@ -82,7 +82,7 @@ class _WebHomePageState extends State<WebHomePage> {
                        gradientStartColor: AppColors.darkGreen2,
                        gradientEndColor: AppColors.darkGreen,
                       iconRoute: "images/800px-Circle_-_black_simple.svg.png",
-                      redirectScreen: LoginPage(),
+                      redirectScreen: AppLoginPage(),
                     ),
                   ],
                 ),
