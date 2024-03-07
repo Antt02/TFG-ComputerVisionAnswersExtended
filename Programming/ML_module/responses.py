6f98a5da79f163e20422f3db2d60e966e1360e3f
Alex Stoia <alexstoia.as@gmail.com>
2024-03-07 10:58:09 +0100

    Sorted answers

--- Programming/ML_module/responses.py
+++ Programming/ML_module/responses.py
@@ -6,6 +6,7 @@ import numpy as np
 width = 800
 height = 1200
 cropped_height = height//3
+possible_answers = [0, 0, 2, 10, 3, 0, 0, 6, 6, 3, 3, 5]
 
 global img_contours
 global img_contours2
@@ -32,7 +33,7 @@ def process_image(answers):
 
 # --- CONTOURS DETECTION ---
 def get_contours(answers):
-    contours, hierarchy = cv2.findContours(answers, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(answers, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(img_contours, contours, -1, (0,255,0), 3)
 
     rectangles = utlis.rectContour(contours)
@@ -40,11 +41,16 @@ def get_contours(answers):
     rectangles_arr = []
     for rect in rectangles:
         rectangles_arr.append(utlis.getCornerPoints(rect))
+        print("----------")
+        print(utlis.getCornerPoints(rect))
+    
+
+    rectangles_arr = order_responses(rectangles_arr)
     
 
     corrected_rect = []
     for rect in rectangles_arr:
-        rect = utlis.reorder(rect)
+        rect = utlis.reshape(rect)
         pt1 = np.float32(rect)
         pt2 = np.float32(np.float32([[0,0],[400,0], [0,cropped_height],[400,cropped_height]]))
         matrix = cv2.getPerspectiveTransform(pt1, pt2)
@@ -52,12 +58,16 @@ def get_contours(answers):
         corrected_rect.append(cv2.warpPerspective(original, matrix, (400, cropped_height)))
 
     for rect in corrected_rect:
+        print("Showing Corrected Rect")
         cv2.imshow('rect', rect)
         cv2.waitKey(0)
+        
 
 
     for rect in rectangles:
         cv2.drawContours(img_contours2, [rect], -1, (0,255,0+len(rect)), 4)
+        #print("##################")
+        #print(rect)
 
     image_array = ([original, img_gray, img_blur, img_canny],
                 [img_contours, img_contours2, np.zeros_like(answers), np.zeros_like(answers)])
@@ -65,14 +75,22 @@ def get_contours(answers):
 
     cv2.imshow("Stacked", img_stacked)
     cv2.waitKey(0)
-        
 
+
+def order_responses(rectangles):
+    rectangulos_ordenados = sorted(rectangles, key=utlis.get_top_left_corner)
+    return rectangulos_ordenados
+        
+#--- Response detection
+def get_response(answer):
+    pass
 
 if __name__ == "__main__":
-    image_path = "Images\Original\G146NFORC-064-Queestionari-dAvaluacio_page-0001.jpg"
+    image_path = "./Images/numbered.jpg"
 
     answers = load_image(image_path)
     img_contours = answers.copy()
     img_contours2 = answers.copy()
     clear_answers = process_image(answers)
+    
     get_contours(clear_answers)
